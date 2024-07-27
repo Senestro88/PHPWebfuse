48dff48187173b35db5c3f6c9132c19f94c09e33
Senestro <Senestro88@gmail.com>
2024-07-27 22:43:47 +0100

    Updated version

---
+++ src/MultiAuth/QrCodeImage.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace PHPWebfuse\MultiAuth;
+
+class QrCodeImage extends \PHPWebfuse\Methods
+{
+    // PRIVATE VARIABLES
+
+    /**
+     * @var \PHPWebfuse\Path The default PHPWebfuse path class
+     */
+    private $path = null;
+
+    /**
+     * @var \PHPWebfuse\Methods The default PHPWebfuse methods class
+     */
+    private $methods = null;
+    
+    // PUBLIC METHODS
+
+    public function __construct()
+    {
+        $this->methods = new \PHPWebfuse\Methods();
+        $this->path = new \PHPWebfuse\Path();
+    }
+
+    /**
+     * Generates a URL that is used to show a QR code.
+     *
+     * @param string $accountName The account name to show and identify
+     * @param string $secret The secret is the generated secret unique to that user
+     * @param string|null $issuer Where you log in to
+     */
+    public function createBase64Image(string $accountName, string $secret, ?string $issuer = null): string
+    {
+        if ($accountName === "" || strpos($accountName, ':') !== false) {
+            throw \PHPWebfuse\MultiAuth\QrException::InvalidAccountName($accountName);
+        }
+        if ($secret === "") {
+            throw \PHPWebfuse\MultiAuth\QrException::InvalidSecret();
+        }
+        $label = $accountName;
+        $content = 'otpauth://totp/%s?secret=%s';
+        if ($issuer !== null) {
+            if ($issuer === "" || strpos($issuer, ':') !== false) {
+                throw \PHPWebfuse\MultiAuth\QrException::InvalidIssuer($issuer);
+            }
+            // Use both the issuer parameter and label prefix as recommended by Google for BC reasons
+            $label = $issuer . ':' . $label;
+            $content .= '&issuer=%s';
+        }
+        $content = htmlspecialchars_decode(sprintf($content, $label, $secret, $issuer));
+        if (!defined('QR_MODE_NUL')) {
+            $this->methods->loadLib("phpqrcode" . DIRECTORY_SEPARATOR . "qrlib");
+        }
+        $tempPathname = $this->path->insert_dir_separator($this->path->arrange_dir_separators(PHPWebfuse['directories']['data'] . DIRECTORY_SEPARATOR . 'multiauth' . DIRECTORY_SEPARATOR . 'temp'));
+        if ($this->methods->makeDir($tempPathname) && class_exists('\QRcode')) {
+            $absolutePath = $tempPathname . '' . $this->methods->randUnique("key") . '.png';
+            \QRcode::png($content, $absolutePath, QR_ECLEVEL_Q, 20, 2);
+            if ($this->methods->isFile($absolutePath)) {
+                clearstatcache(false, $absolutePath);
+                $mime = mime_content_type($absolutePath);
+                $baseEncode = base64_encode((string) $this->methods->getFileContent($absolutePath));
+                $data = 'data:' . $mime . ';base64,' . $baseEncode;
+                $this->methods->deleteFile($absolutePath);
+                return $data;
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Display the QR code to browser
+     *
+     * @param string $accountName The account name to show and identify
+     * @param string $secret The secret is the generated secret unique to that user
+     * @param string|null $issuer Where you log in to
+     */
+    public function createOuputImage(string $accountName, string $secret, ?string $issuer = null): void
+    {
+        if ($accountName === "" || strpos($accountName, ':') !== false) {
+            throw \PHPWebfuse\MultiAuth\QrException::InvalidAccountName($accountName);
+        }
+        if ($secret === "") {
+            throw \PHPWebfuse\MultiAuth\QrException::InvalidSecret();
+        }
+        $label = $accountName;
+        $content = 'otpauth://totp/%s?secret=%s';
+        if ($issuer !== null) {
+            if ($issuer === "" || strpos($issuer, ':') !== false) {
+                throw \PHPWebfuse\MultiAuth\QrException::InvalidIssuer($issuer);
+            }
+            // Use both the issuer parameter and label prefix as recommended by Google for BC reasons
+            $label = $issuer . ':' . $label;
+            $content .= '&issuer=%s';
+        }
+        $content = htmlspecialchars_decode(sprintf($content, $label, $secret, $issuer));
+        if (!defined('QR_MODE_NUL')) {
+            $this->methods->loadLib("phpqrcode" . DIRECTORY_SEPARATOR . "qrlib");
+        }
+        $tempPathname = $this->path->insert_dir_separator($this->path->arrange_dir_separators(PHPWebfuse['directories']['data'] . DIRECTORY_SEPARATOR . 'multiauth' . DIRECTORY_SEPARATOR . 'temp'));
+        if ($this->methods->makeDir($tempPathname) && class_exists('\QRcode')) {
+            $absolutePath = $tempPathname . '' . $this->methods->randUnique("key") . '.png';
+            \QRcode::png($content, $absolutePath, QR_ECLEVEL_Q, 4, 2);
+            if ($this->methods->isFile($absolutePath)) {
+                $image = imagecreatefrompng($absolutePath);
+                if ($this->methods->isNotFalse($image) && !headers_sent()) {
+                    $contentType = mime_content_type($absolutePath);
+                    header("Expires: Mon, 7 Apr 1997 01:00:00 GMT");
+                    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . "GMT");
+                    header("Cache-Control: no-store, no-cache, must-revalidate");
+                    header("Cache-Control: post-check=0, pre-check=0", false);
+                    header("Content-Type: " . $contentType);
+                    header("Pragma: no-cache");
+                    imagepng($image, null, 9);
+                    imagedestroy($image);
+                }
+            }
+        }
+    }
+}
---
+++ src/Time.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace PHPWebfuse;
+
+/**
+ * The PHPWebfuse 'Time' class
+ */
+class Time
+{
+    // PUBLIC METHODS
+
+    /**
+     * Construct new Time instance
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Returns an associative array containing the date information of the timestamp, or the current local time if timestamp is omitted or null.
+     * @param int|null $timestamp
+     * @return array
+     */
+    public function getDate(?int $timestamp = null): array
+    {
+        return getdate($timestamp);
+    }
+
+    /**
+     * Organize a time unix timestamp
+     * @param ?int  $timestamp
+     * @return string
+     */
+    public function organizeTime(?int $timestamp = null): string
+    {
+        $currentDate = $this->getDate();
+        $targetDate = $this->getDate($timestamp);
+        if ($currentDate['mday'] === $targetDate['mday'] && $currentDate['mon'] === $targetDate['mon'] && $currentDate['year'] === $targetDate['year']) {
+            return date("h:i A", $timestamp);
+        } elseif (($currentDate['mday'] - 1) === $targetDate['mday'] && $currentDate['mon'] === $targetDate['mon'] && $currentDate['year'] === $targetDate['year']) {
+            return "Yesterday";
+        } else {
+            $isYear = ($currentDate['year'] !== $targetDate['year']) ? ", Y" : '';
+            return ucfirst(date("F j" . $isYear, $timestamp));
+        }
+    }
+
+    /**
+     * Format a time unix timestamp into time ago
+     * @param int $timestamp:
+     * @return string
+     */
+    public function FormatTimeAgoVersion1(int $timestamp): string
+    {
+        $difference = time() - $timestamp;
+        if ($difference < 5) {
+            return 'less than 5 seconds ago';
+        }
+        $conditions = [31104000 => 'year', 2592000 => 'month', 86400 => 'day', 3600 => 'hour', 60 => 'minute', 1 => 'second'];
+        foreach ($conditions as $seconds => $unit) {
+            $differenceUnit = floor($difference / $seconds);
+            if ($differenceUnit >= 1) {
+                $plural = $differenceUnit > 1 ? 's' : '';
+                return $differenceUnit . ' ' . $unit . $plural . ' ago';
+            }
+        }
+        return '..';
+    }
+
+    /**
+     * Format a time unix timestamp into time ago
+     * @param int $timestamp
+     * @return string
+     */
+    public function FormatTimeAgoVersion2(int $timestamp): string
+    {
+        $date = new \DateTime();
+        $date->setTimestamp($timestamp);
+        $dateDiff = $date->diff(new \DateTime());
+        $since = ['year' => $dateDiff->y, 'month' => $dateDiff->m, 'day' => $dateDiff->d, 'hour' => $dateDiff->h, 'minute' => $dateDiff->i, 'second' => $dateDiff->s];
+        $since = array_filter($since); // Remove empty date values
+        $since = array_slice($since, 0, $level); // Output only the first x date values
+        $lastValue = key(array_slice($since, -1, 1, true)); // Build string
+        $string = '';
+        foreach ($since as $unit => $value) {
+            if ($string) {
+                $string .= $unit !== $lastValue ? ', ' : ' and ';
+            }
+            $plural = $value > 1 ? 's' : ''; // Set plural
+            $string .= $value . ' ' . $unit . $plural; // Add date value
+        }
+        return $string . ' ago';
+    }
+}
