f7e34765a1d7e38fed9f12347ee23aceb2e49c61
Senestro <Senestro88@gmail.com>
2024-09-23 13:41:37 +0100

    PHPWebfuse

--- src/Instance/Mail.php
+++ src/Instance/Mail.php
@@ -2,6 +2,7 @@
 
 namespace PHPWebfuse\Instance;
 
+use PHPMailer\PHPMailer\SMTP;
 use \PHPWebfuse\Utils;
 use \PHPWebfuse\File;
 
@@ -63,11 +64,12 @@ class Mail {
      * @param string $message
      * @param array $attachments
      * @param bool $authenticate
+     * @param bool $debug
      * @return bool|string
      * 
      * Return true on success, otherwise false or string representing error message
      */
-    public function sendMail(string $emailFrom, string $emailTo, string $title, string $message, array $attachments = array(), bool $authenticate = true): bool|string {
+    public function sendMail(string $emailFrom, string $emailTo, string $title, string $message, array $attachments = array(), bool $authenticate = true, bool $debug = false): bool|string {
         $result = "";
 
         // Check if PHPMailer class exists
@@ -77,15 +79,11 @@ class Mail {
                 // Reset attachments to only include valid files
                 $attachments = $this->resetAttachments($attachments);
                 // Configure mailer settings
-                $mailer->SMTPDebug = 0;
+                $mailer->SMTPDebug = $debug ? SMTP::DEBUG_CONNECTION : SMTP::DEBUG_OFF;
                 $mailer->isSMTP();
                 $mailer->WordWrap = $this->wordwrap;
                 $mailer->Host = $this->host;
-                $mailer->SMTPAuth = $authenticate;
-                $mailer->SMTPAutoTLS = true;
-                $mailer->Username = $this->username;
-                $mailer->Password = $this->password;
-                $mailer->SMTPSecure = $this->mode;
+                $this->setAuthentication($mailer, $authenticate);
                 $mailer->Port = $this->port;
                 $mailer->SMTPOptions = array('ssl' => array('verify_peer' => $authenticate, 'verify_peer_name' => $authenticate, 'allow_self_signed' => !$authenticate));
                 // Set mail content
@@ -120,7 +118,7 @@ class Mail {
         // Update mail configuration with provided settings
         $this->host = isset($config['host']) && Utils::isNotEmptyString($config['host']) ? (string) $config['host'] : "localhost";
         $this->username = isset($config['username']) && Utils::isNotEmptyString($config['username']) ? (string) $config['username'] : "";
-        $this->password = isset($config['password']) && Utils::isNotEmptyString($config[' password']) ? (string) $config['password'] : "";
+        $this->password = isset($config['password']) && Utils::isNotEmptyString($config['password']) ? (string) $config['password'] : "";
         $this->mode = isset($config['mode']) && Utils::isNotEmptyString($config['mode']) ? (string) $config['mode'] : "tls";
         $this->port = isset($config['port']) && Utils::isNumeric($config['port']) ? (int) $config['port'] : 587;
         $this->wordwrap = isset($config['wordwrap']) && Utils::isNumeric($config['wordwrap']) ? (int) $config['wordwrap'] : 100;
@@ -152,6 +150,22 @@ class Mail {
             if (File::isFile($attachment)) {
                 $mailer->addAttachment($attachment);
             }
+        }
+    }
+
+    /**
+     * Set authentication for the mailer
+     * 
+     * @param \PHPMailer\PHPMailer\PHPMailer $mailer
+     * @param bool $authenticate
+     */
+    private function setAuthentication(\PHPMailer\PHPMailer\PHPMailer $mailer, bool $authenticate = true): void {
+        if ($authenticate) {
+            $mailer->SMTPAuth = true; // Whether to use SMTP authentication.
+            $mailer->SMTPAutoTLS = true; // Whether to enable TLS encryption automatically if a server supports it, even if SMTPSecure is not set to 'tls'.
+            $mailer->Username = $this->username; // SMTP username.
+            $mailer->Password = $this->password; // SMTP password.
+            $mailer->SMTPSecure = $this->mode; // What kind of encryption to use on the SMTP connection.
         }
     }
 }
